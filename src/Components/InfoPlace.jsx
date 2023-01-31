d1b62d50039dc367ee26294ceed6e7a0b21988a5
mashakun <shakunica@gmail.com>
2023-01-31 13:06:08 +0300

    removing

--- src/Components/InfoPlace.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const InfoPlace = () => {
-    return (
-        <div class="InfoPlace">Info place</div>
-    );
-};
-
-export default InfoPlace;
--- src/Components/ListPlace.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ListPlace = () => {
-    return (
-        <div class="ListPlace">List place</div>
-    );
-};
-
-export default ListPlace;
